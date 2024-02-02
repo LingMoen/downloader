022ee635e93bce16697cb05f74532888c69e351a
Ling Moen <136664622+LingMoen@users.noreply.github.com>
2024-02-02 11:41:42 +0700

    update index.js

--- index.js
+++ index.js
@@ -4,23 +4,20 @@ import fs from 'fs';
 import os from 'os';
 import path from 'path';
 import cheerio from 'cheerio';
-
-import {
-	fileURLToPath
-} from "url"
-const __dirname = path.dirname(fileURLToPath(import.meta.url))
+const PORT = process.env.PORT || 3000;
 const app = express();
-const port = process.env.PORT || 3000; // Menggunakan variabel PORT dari lingkungan jika tersedia
-const hostname = process.env.HOSTNAME || 'localhost'; // Ganti 'localhost' sesuai dengan hostname Anda
 
+const tempDir = path.join(os.tmpdir(), "temp");
 
-const tempikDir = path.join(__dirname, 'tmp');
-app.use('/tmp', express.static('/tmp'));
-
+// Create temp directory if it doesn't exist
+if (!fs.existsSync(tempDir)) {
+  fs.mkdirSync(tempDir, { recursive: true });
+}
+app.use('/temp', express.static(tempDir));
+app.use(express.json());
 
 app.get("/", (req, res) => {
   res.type("json");
-
   const keluaran = {
     success: true,
     author: "Nex",
@@ -31,123 +28,12 @@ app.get("/", (req, res) => {
   res.send(keluaran);
 });
 
-const generateRandomIP = () => {
-  const octet = () => Math.floor(Math.random() * 256);
-  return `${octet()}.${octet()}.${octet()}.${octet()}`;
-};
-
-const checkMediaType = (url) => {
-  const supportedImageExtensions = ['jpg', 'jpeg', 'png', 'webp'];
-  const supportedVideoExtensions = ['mp4'];
-  const fileExtension = url.toLowerCase();
-  if (supportedImageExtensions.includes(fileExtension)) {
-    return 'image';
-  } else if (supportedVideoExtensions.includes(fileExtension)) {
-    return 'video';
-  } else {
-    return 'unknown';
-  }
-};
-
-function getHostname(){
-  const url = "https://downloader-nex.vercel.app";
-  return url.includes('http') ? url : `https://${url}`
-}
-
-
-const getMimeTypeFromUrl = async (url) => {
-  try {
-    const response = await axios.head(url);
-    const contentTypeHeader = response.headers['content-type'];
-    const [mediaType] = contentTypeHeader.split('/');
-    return mediaType;
-  } catch (error) {
-    console.error('Error:', error.message);
-  }
-};
-//end tools
-
-const igdl1 = async (instagramUrl) => {
-  try {
-    const base_url = "https://www.save-free.com/";
-    const headers = {
-      'Content-Type': 'application/x-www-form-urlencoded; charset=UTF-8',
-      'Accept': 'text/html, */*; q=0.01',
-      'X-Valy-Cache': 'accpted',
-      'X-Requested-With': 'XMLHttpRequest',
-      'User-Agent': 'Mozilla/5.0 (iPhone14,3; U; CPU iPhone OS 15_0 like Mac OS X) AppleWebKit/602.1.50 (KHTML, like Gecko) Version/10.0 Mobile/19A346 Safari/602.1',
-      'Referer': 'https://www.save-free.com/',
-      'X-Forwarded-For': generateRandomIP(),
-    };
-
-    const postData = `instagram_url=${encodeURIComponent(instagramUrl)}&type=media&resource=save`;
-
-    const response = await axios.post(`${base_url}process`, postData, { headers });
-
-    const result = response.data.map(item => ({
-      title: item.meta.title,
-      urls: item.url.map(urlItem => ({
-        url: urlItem.url,
-        type: checkMediaType(urlItem.type)
-      }))
-    }));
-
-    return result;
-  } catch (error) {
-    console.error(error);
-    throw error;
-  }
-};
-
-
-const igdl2 = async (instagramUrl) => {
-  const headers = {
-    'Accept': 'application/json, text/plain, */*',
-    'Content-Type': 'application/json',
-    'X-XSRF-TOKEN': 'eyJpdiI6InNzNVlEQVJxcVYyVkdvWURBTGNad1E9PSIsInZhbHVlIjoiZkxNOGRKYXREdkVLZWRQeWp1bUFadG5BYWhOTzdoWDlXQ2FaQVwvV2NnZkQ2TzcyYVRuMlNRUkVHam1Nc29KTGRCam16U2xFODZFbkFvYldmdkxUUSt5ZHRyWUljNHZjWk9HZUp4elBxaHFRcGxIUGlxS1hQZUtmTVRoMkxjNVJ1IiwibWFjIjoiOGI0YTA4NmIyODgzZGJmMmIwNzU2MGRiYzk4YjQ5OGUzMzYwZGRiZjEzYjdlYjVjM2NhNTZjMGJiMWViMGU3YSJ9',
-    'User-Agent': 'Mozilla/5.0 (Linux; Android 12; SM-A9080 Build/SP1A.210812.016; wv) AppleWebKit/537.36 (KHTML, like Gecko) Version/4.0 Chrome/119.0.6045.17 Mobile Safari/537.36 [FB_IAB/FB4A;FBAV/436.0.0.35.101;]',
-    'Referer': 'https://snapinst.com/'
-  };
-
-  const data = {
-    url: instagramUrl,
-    ts: Date.now(),
-    _ts: Date.now(),
-    _tsc: 0,
-    _s: '045b2f13f32f465471a3cd1af4c63f83b6e50cb54c5c642e3953ef152ef1fea6'
-  };
-
-  try {
-    const response = await axios.post('https://snapinst.com/api/convert', data, { headers });
-    
-    const downloadLinks = [];
-    let dataArray = response.data; // Declare dataArray using let
-    
-    await Promise.all(dataArray.map(async (item) => {
-      await Promise.all(item.url.map(async (urlItem) => {
-        const urlString = urlItem.url;
-        const tipe = await getMimeTypeFromUrl(urlString);
-        downloadLinks.push({ title: item.meta.title, urls: [{ url: urlString, type: tipe }] });
-      }));
-    }));
-
-    return downloadLinks;
-  } catch (error) {
-    console.error(error);
-  }
-};
-
-
-
 async function downloadImage(url) {
   try {
     const response = await axios.get(url, { responseType: 'arraybuffer' });
-    const tempDir = path.join(__dirname, 'tmp'); // Assuming "tmp" is in the same directory as your script
     const randomCode = Math.random().toString(36).substring(7);
-    const imagePath = `/tmp/downloaded_image_${randomCode}.png`;
-
-    fs.writeFileSync(imagePath, Buffer.from(response.data, 'binary'));
-
+    const imagePath = `downloaded_image_${randomCode}.png`;
+    fs.writeFileSync(path.join(tempDir, "/" + imagePath), Buffer.from(response.data, 'binary'));
     return imagePath;
   } catch (error) {
     console.error('Error downloading image:', error.message);
@@ -158,12 +44,9 @@ async function downloadImage(url) {
 async function downloadVideo(url) {
   try {
     const response = await axios.get(url, { responseType: 'arraybuffer' });
-    const tempDir = path.join(__dirname, 'tmp'); // Assuming "tmp" is in the same directory as your script
     const randomCode = Math.random().toString(36).substring(7);
-    const videoPath = `/tmp/downloaded_video_${randomCode}.mp4`;
-
-    fs.writeFileSync(videoPath, Buffer.from(response.data, 'binary'));
-
+    const videoPath = `downloaded_video_${randomCode}.mp4`;
+    fs.writeFileSync(path.join(tempDir, "/" + videoPath), Buffer.from(response.data, 'binary'));
     return videoPath;
   } catch (error) {
     console.error('Error downloading video:', error.message);
@@ -171,86 +54,286 @@ async function downloadVideo(url) {
   }
 }
 
+const generateRandomIP = () => {
+	const octet = () => Math.floor(Math.random() * 256);
+	return `${octet()}.${octet()}.${octet()}.${octet()}`;
+};
+
+const getMimeTypeFromUrl = async (url) => {
+	try {
+		const headers = {
+			'Content-Type': 'application/x-www-form-urlencoded; charset=UTF-8',
+			'Accept': '*/*',
+			'User-Agent': 'Mozilla/5.0 (Linux; Android 12; SM-S908B Build/SP1A.210812.016; wv) AppleWebKit/537.36 (KHTML, like Gecko) Version/4.0 Chrome/99.0.4844.58 Mobile Safari/537.36 [FB_IAB/FB4A;FBAV/357.0.0.23.115;]',
+			'Referer': 'https://igdownloader.app/en',
+			'X-Forwarded-For': generateRandomIP()
+		};
+
+		const response = await axios.head(url, {
+			headers
+		});
+		const contentTypeHeader = response.headers['content-type'];
+		const [mediaType] = contentTypeHeader.split('/');
+		return mediaType;
+	} catch (error) {
+		console.error('Error while fetching media type:', error.message);
+		return undefined;
+	}
+};
+
+const checkMediaType = (url) => {
+	const supportedImageExtensions = ['jpg', 'jpeg', 'png', 'webp', 'heic'];
+	const supportedVideoExtensions = ['mp4'];
+	const fileExtension = url.toLowerCase();
+	if (supportedImageExtensions.includes(fileExtension)) {
+		return 'image';
+	} else if (supportedVideoExtensions.includes(fileExtension)) {
+		return 'video';
+	} else {
+		return 'unknown';
+	}
+};
 
 
-// Middleware untuk mengizinkan akses ke direktori os.tmpdir()
+/*
+IGDL IGDL IGDL IGDL IGDL IGDL
+IGDL IGDL IGDL IGDL IGDL IGDL
+*/
+
+async function igdl1(url) {
+	try {
+		const apiEndpoint = 'https://v3.igdownloader.app/api/ajaxSearch';
+		const requestOptions = {
+			headers: {
+				'Content-Type': 'application/x-www-form-urlencoded; charset=UTF-8',
+				'Accept': '*/*',
+				'User-Agent': 'Mozilla/5.0 (Linux; Android 12; SM-S908B Build/SP1A.210812.016; wv) AppleWebKit/537.36 (KHTML, like Gecko) Version/4.0 Chrome/99.0.4844.58 Mobile Safari/537.36 [FB_IAB/FB4A;FBAV/357.0.0.23.115;]',
+				'Referer': 'https://igdownloader.app/en',
+				'X-Forwarded-For': generateRandomIP()
+			},
+		};
+		const postData = `recaptchaToken=&q=${encodeURIComponent(url)}&t=media&lang=en`;
+		const response = await axios.post(apiEndpoint, postData, requestOptions);
+		const $ = cheerio.load(response.data.data);
+		const downloadLinks = $('div.download-items__btn > a');
+		const hrefArray = {
+			title: null,
+			urls: []
+		}
+		await Promise.all(downloadLinks.map(async (index, element) => {
+			const href = $(element).attr('href');
+			const media_type = await getMimeTypeFromUrl(href);
+			let type;
+			if (typeof media_type === 'undefined') {
+				type = "video";
+			} else {
+				type = media_type;
+			}
+			hrefArray.urls.push({
+				url: href,
+				type: type
+			});
+		}));
+		return hrefArray;
+	} catch (error) {
+		console.error('Instagram Downloader 1 - Error:', error.message);
+		return null;
+	}
+}
+
+async function igdl2(url) {
+	const apiEndpoint = 'https://snapinst.com/api/convert';
+
+	const requestData = {
+		url,
+		ts: Date.now(),
+		_ts: Date.now() - 106169240745,
+		_tsc: 0,
+		_s: 'b4d95f81de50ed9cace0103923a25dd2f57b2a76c142d82ac78a963f1274a1e1',
+	};
+
+	const headers = {
+		'Accept': 'application/json, text/plain, */*',
+		'Content-Type': 'application/json',
+		'X-XSRF-TOKEN': 'eyJpdiI6InZ3aU9SVG41enJWOUljS3hIUnpsd3c9PSIsInZhbHVlIjoiakFHQjcrVjNMQm1wZ2xrcmF6NGdOSjdTUFp0ZTNFNXpCZ0tcL3VaNmFaN09TSVl2QzFZVndTVmxLUFo2QVVuYnpcL2JDdVwvc29sdHB6XC9jSzY2aURYMDdzcmd4TWVHUjZzWHpHZXEySXJMb0UwN3dqbUFDRlZFTXFxU2E4U2hOUzg5IiwibWFjIjoiNGQyYjBjYWNhNGQwMWUwZWE4YWM1MzdkMWJlYmRkYzBkOTMyZDZjNWVhNjE3MzAxOWMwNTM2OTJiOTM0ZjMwNyJ9',
+		'User-Agent': 'Mozilla/5.0 (Linux; Android 10; K) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Mobile Safari/537.36',
+		'Referer': 'https://snapinst.com/',
+	};
+
+	try {
+		const response = await axios.post(apiEndpoint, requestData, {
+			headers
+		});
+		const resultArray = [];
+
+		const transformedData = {
+			title: response.data[0].meta.title,
+			urls: response.data.map(item => item.url.map(urlInfo => ({
+				url: urlInfo.url,
+				type: checkMediaType(urlInfo.type)
+			})))
+		};
+
+		return transformedData;
+	} catch (error) {
+		console.error('Instagram Downloader 2 - Error:', error.message);
+		return null;
+	}
+}
 
 
-app.use(express.json());
+async function igdl3(url) {
+	const apiEndpoint = 'https://co.wuk.sh/api/json';
 
-// ... (kode sebelumnya)
+	const requestData = {
+		url,
+		aFormat: 'mp3',
+		filenamePattern: 'classic',
+		dubLang: false,
+		vQuality: '720',
+	};
+
+	const headers = {
+		'Accept': 'application/json',
+		'Content-Type': 'application/json',
+		'User-Agent': 'Mozilla/5.0 (Linux; Android 10; K) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Mobile Safari/537.36',
+		'Referer': 'https://cobalt.tools/',
+	};
+
+	try {
+		const response = await axios.post(apiEndpoint, requestData, {
+			headers
+		});
+		var result = response.data;
+		let array_res = {
+			title: null,
+			urls: []
+		}
+		if (result.status === "redirect") {
+			let media_type = await getMimeTypeFromUrl(result.url)
+			array_res.urls.push({
+				url: result.url,
+				type: media_type
+			})
+		} else if (result.status === "picker") {
+			for (let i = 0; i < result.picker.length; i++) {
+				let media_type = await getMimeTypeFromUrl(result.picker[i].url)
+				array_res.urls.push({
+					url: result.picker[i].url,
+					type: media_type
+				})
+			}
+		}
+		return array_res;
+	} catch (error) {
+		console.error('Instagram Downloader 3 - Error:', error.message);
+		return null;
+	}
+};
+
+async function igdl4(url) {
+	try {
+		const apiEndpoint = 'https://v3.saveig.app/api/ajaxSearch';
+		const requestOptions = {
+			headers: {
+				'Content-Type': 'application/x-www-form-urlencoded; charset=UTF-8',
+				'Accept': '*/*',
+				'User-Agent': 'Mozilla/5.0 (Linux; Android 12; SM-S908B Build/SP1A.210812.016; wv) AppleWebKit/537.36 (KHTML, like Gecko) Version/4.0 Chrome/99.0.4844.58 Mobile Safari/537.36 [FB_IAB/FB4A;FBAV/357.0.0.23.115;]',
+				'Referer': 'https://saveig.app/en',
+			},
+		};
+		const postData = `recaptchaToken=&q=${encodeURIComponent(url)}&t=media&lang=en`;
+		const response = await axios.post(apiEndpoint, postData, requestOptions);
+		const $ = cheerio.load(response.data.data);
+		const downloadLinks = $('div.download-items__btn > a');
+		const hrefArray = {
+			title: null,
+			urls: []
+		}
+		await Promise.all(downloadLinks.map(async (index, element) => {
+			const href = $(element).attr('href');
+			const media_type = await getMimeTypeFromUrl(href);
+			let type;
+			if (typeof media_type === 'undefined') {
+				type = "video";
+			} else {
+				type = media_type;
+			}
+			hrefArray.urls.push({
+				url: href,
+				type: type
+			});
+		}));
+		return hrefArray;
+	} catch (error) {
+		console.error('Instagram Downloader 4 - Error:', error.message);
+		return null;
+	}
+}
+
+const getInstagramDownloadLinks = async (url) => {
+  let result = await igdl1(url);
+  if (!result) {
+    result = await igdl2(url);
+  }
+  if (!result) {
+    result = await igdl3(url);
+  }
+  if (!result) {
+    result = await igdl4(url);
+  }
+  if (!result) {
+    result = {
+      message: "all server error"
+    };
+  }
+  return result;
+};
 
 app.get('/igdl', async (req, res) => {
   try {
     const { url } = req.query;
-
     if (!url) {
       return res.status(400).json({ error: 'Parameter url is required' });
     }
-
     if (!/https?:\/\/(www\.)?instagram\.com\/(p|reel|tv)/.test(url)) {
       return res.status(400).json({ error: "Example: https://www.instagram.com/p/Cz1fTwMJFpx/?igsh=MXRrY2g4eWNucGoyZg==" });
     }
+    let result = await getInstagramDownloadLinks(url);
+    let result_upload = { 
+      title: result?.title || 'untitled',
+      media: []
+    }
 
-    let result;
-
-    try {
-      result = await igdl1(url);
-    } catch (error1) {
-      // If igdl1 encounters an error, try igdl2
-      console.error("igdl1 error:", error1);
-
-      try {
-        result = await igdl2(url);
-      } catch (error2) {
-        console.error("igdl2 error:", error2);
-        return res.status(400).json({ error: error2 });
+    for (let item of result.urls) {
+      if (item.type === "image") {
+        let unduh = await downloadImage(item.url);
+        result_upload.media.push({ type: item.type, path: unduh, url_path: `https://downloader-nex.vercel.app/temp/${path.basename(unduh)}` });
+      } else if (item.type === "video") { 
+        let unduh = await downloadVideo(item.url);
+        result_upload.media.push({ type: item.type, path: unduh, url_path: `https://downloader-nex.vercel.app/temp/${path.basename(unduh)}` });
       }
     }
-
-    if (!result || result.length === 0) {
-      return res.status(400).json({ error: "No media found on the provided Instagram URL." });
-    }
-
-    const array_media = [];
-
-    for (const item of result) {
-      const { title, urls } = item;
-      let captions = `Title: ${title}\n`;
-
-      for (const urlItem of urls) {
-        const { url, type } = urlItem;
-
-        if (type === 'image') {
-          let path_img = await downloadImage(url);
-          array_media.push({ path: path_img, caption: captions, url: `${getHostname()}/tmp/${path.basename(path_img)}` });
-        } else if (type === 'video') {
-          let path_vid = await downloadVideo(url);
-          array_media.push({ path: path_vid, caption: captions, url: `${getHostname()}/tmp/${path.basename(path_vid)}` });
-        }
-      }
-    }
-    // Hapus file setelah respons dikirim
-    res.json(array_media);
-    // Ambil path file dari array_media dan hapus setelah respons dikirim
-    array_media.forEach(async (item) => {
+    
+    res.json(result_upload);
+    
+    for (let item of result_upload.media) {
       try {
-        // Hapus file setelah 10 menit
-        setTimeout(async () => {
-          await fs.promises.unlink(item.path);
-          console.log(`File ${item.path} deleted.`);
-        }, 10 * 60 * 1000); // 10 menit
+        await new Promise(resolve => setTimeout(resolve, 10 * 60 * 1000)); // 10 minutes
+        await fs.unlink(item.path);
+        console.log(`File ${item.path} deleted.`);
       } catch (error) {
         console.error(`Error deleting file ${item.path}:`, error);
       }
-    });
-    
+    }
   } catch (error) {
     console.error('Error processing request:', error);
     res.status(500).json({ error: 'Failed to process request\n' + error});
   }
 });
 
-app.listen(port, () => {
-  console.log(`Server is running on http://${hostname}:${port}`);
+
+const hostname = `https://downloader-nex.vercel.app`;  // Ganti dengan host yang sesuai
+app.listen(PORT, () => {
+  console.log(`Server is running on http://${hostname}:${PORT}`);
 });
