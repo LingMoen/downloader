0c4debcba5e7904a11a7be24ba403cec92e3d598
Ling Moen <136664622+LingMoen@users.noreply.github.com>
2024-02-08 21:10:30 +0700

    update index.js

--- index.js
+++ index.js
@@ -315,7 +315,7 @@ app.get('/igdl', async (req, res) => {
       }
     }
     
-    res.json(JSON.stringify(result_upload, null, 2));
+    res.json(result_upload);
     
     for (let item of result_upload.media) {
       try {
